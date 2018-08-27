20f847713d9d464068d49f7537907de48e01cefa
Wilna L Bean <wilna.bean@up.ac.za>
2018-08-27 14:26:33 +0200

    Added CollaborationStatusMutator.java.

---
+++ src/main/java/receiver/replanning/CollaborationStatusMutator.java
@@ -0,0 +1,49 @@
+package receiver.replanning;
+
+import org.matsim.core.replanning.ReplanningContext;
+import org.matsim.core.replanning.modules.GenericPlanStrategyModule;
+
+import receiver.ReceiverPlan;
+
+/**
+ * This is a class that changes a receiver's collaboration status during replanning.
+ * @author wlbean
+ *
+ */
+
+public class CollaborationStatusMutator implements GenericPlanStrategyModule<ReceiverPlan> {
+	
+	/*
+	 * A class that changes a receiver's collaboration status.
+	 */
+	
+	public CollaborationStatusMutator(){
+		
+	}
+
+	@Override
+	public void prepareReplanning(ReplanningContext replanningContext) {
+
+	}
+
+	@Override
+	public void handlePlan(ReceiverPlan receiverPlan) {
+		boolean newstatus;
+		boolean status = receiverPlan.getReceiver().getCollaborationStatus();
+		
+		if (status == true){
+			 newstatus = false;
+		} else {
+			newstatus = true;
+		}
+		
+		receiverPlan.getReceiver().setCollaborationStatus(newstatus);
+
+	}
+
+	@Override
+	public void finishReplanning() {
+
+	}
+
+}
