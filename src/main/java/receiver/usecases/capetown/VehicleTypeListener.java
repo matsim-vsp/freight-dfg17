dbb961ce92a676c8663212af9a7527abd3d3044b
Wilna L Bean <wilna.bean@up.ac.za>
2018-11-15 10:31:48 +0200

    Updated VehicleTypeListener imports.

--- src/main/java/receiver/usecases/capetown/VehicleTypeListener.java
+++ src/main/java/receiver/usecases/capetown/VehicleTypeListener.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.freight.carrier.Carrier;
