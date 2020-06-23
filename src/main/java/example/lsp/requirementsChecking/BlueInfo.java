57c428682a2b9cf46baf544781cbf0c236413030
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2020-06-23 12:17:54 +0200

    move lsp example in own subpackage

---
+++ src/main/java/example/lsp/requirementsChecking/BlueInfo.java
@@ -0,0 +1,49 @@
+package example.lsp.requirementsChecking;
+
+import lsp.functions.*;
+
+public class BlueInfo extends Info{
+
+private InfoFunction blueInfoFunction;
+	
+	public BlueInfo() {
+		blueInfoFunction = InfoFunctionUtils.createDefaultInfoFunction();
+		InfoFunctionValue<String> value = InfoFunctionUtils.createInfoFunctionValue("blue" );
+		value.setValue("blue");
+		blueInfoFunction.getValues().add(value);
+	}
+	
+	@Override
+	public String getName() {
+		return "blue";
+	}
+
+	@Override
+	public InfoFunction getFunction() {
+		return blueInfoFunction;
+	}
+
+	@Override
+	public double getFromTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getToTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/main/java/example/lsp/requirementsChecking/BlueRequirement.java
@@ -0,0 +1,19 @@
+package example.lsp.requirementsChecking;
+
+import lsp.functions.Info;
+import lsp.LogisticsSolution;
+import lsp.shipment.Requirement;
+
+public class BlueRequirement implements Requirement{
+
+	@Override
+	public boolean checkRequirement(LogisticsSolution solution) {
+		for(Info info : solution.getInfos()) {
+			if(info instanceof BlueInfo) {
+				return true;
+			}
+		}	
+		return false;
+	}
+
+}
