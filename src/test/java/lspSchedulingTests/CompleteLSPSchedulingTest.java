9feb74983cae9e5590552c24a76ffb2a8820317d
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2020-07-06 16:51:57 +0200

    reduce public footprint of lsp.usecase.DistributionCarrierScheduler

--- src/test/java/lspSchedulingTests/CompleteLSPSchedulingTest.java
+++ src/test/java/lspSchedulingTests/CompleteLSPSchedulingTest.java
@@ -174,8 +174,7 @@ public class CompleteLSPSchedulingTest {
 		LSPUtils.LogisticsSolutionElementBuilder secondReloadingElementBuilder = LSPUtils.LogisticsSolutionElementBuilder.newInstance(secondReloadingElementId );
 		secondReloadingElementBuilder.setResource(secondReloadingPointAdapter);
 		secondReloadElement = secondReloadingElementBuilder.build();
-		
-		DistributionCarrierScheduler distriutionScheduler = new DistributionCarrierScheduler();
+
 		Id<Carrier> distributionCarrierId = Id.create("DistributionCarrier", Carrier.class);
 		Id<VehicleType> distributionVehicleTypeId = Id.create("DistributionCarrierVehicleType", VehicleType.class);
 		CarrierVehicleType.Builder dsitributionVehicleTypeBuilder = CarrierVehicleType.Builder.newInstance(distributionVehicleTypeId);
@@ -202,7 +201,7 @@ public class CompleteLSPSchedulingTest {
 		
 		Id<Resource> distributionAdapterId = Id.create("DistributionCarrierAdapter", Resource.class);
 		DistributionCarrierAdapter.Builder distributionAdapterBuilder = DistributionCarrierAdapter.Builder.newInstance(distributionAdapterId, network);
-		distributionAdapterBuilder.setDistributionScheduler(distriutionScheduler);
+		distributionAdapterBuilder.setDistributionScheduler(UsecaseUtils.createDefaultDistributionCarrierScheduler());
 		distributionAdapterBuilder.setCarrier(distributionCarrier);
 		distributionAdapterBuilder.setLocationLinkId(distributionLinkId);
 		distributionAdapter = distributionAdapterBuilder.build();
