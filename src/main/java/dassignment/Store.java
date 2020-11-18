b47879222bcbd7f42c72bb984688b6ee2f4033a0
kainagel <nagel@vsp.tu-berlin.de>
2020-11-18 15:28:55 +0100

    dassignment code maintenance

--- src/main/java/dassignment/Store.java
+++ src/main/java/dassignment/Store.java
@@ -5,7 +5,6 @@
 package dassignment;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Random;
 
 class Store
@@ -23,26 +22,35 @@ class Store
 		this.demand = demand;
 	}
 
-	public Store()
+	private final String id;
+	private final double x;
+	private final double y;
+	/**
+	 *
+	 */
+	private final String labelA;
+	/**
+	 * So-called O label: stores are mandatorily assigned to distribution centers with same O label.
+	 */
+	private final String labelO;
+	/**
+	 * Stores are mandatorily assigned to distribution centers with same H label.  However, DCs can serve stores with different H label (which can
+	 * only be triggered when the DC has a wildcard H label).  A store with a * label cannot be assigned to a DC with a non-wildcard label.  (????)
+	 * See configMatchList below for how it is coded.
+	 */
+	private final String labelH;
+	/**
+	 * Stores are assigned preferably to DCs with same P label.
+	 */
+	private final String labelP;
+	private final double demand;
+	private final ArrayList<DistributionCenter> highPriorityMatchList = new ArrayList<>();
+	private final ArrayList<DistributionCenter> lowPriorityMatchList = new ArrayList<>();
+	private DistributionCenter selectedDistributionCenter;
+
+	public DistributionCenter getSelectedDistributionCenter()
 	{
-
-	}
-
-	private String id;
-	private double x;
-	private double y;
-	private String labelA;
-	private String labelO;
-	private String labelH;
-	private String labelP;
-	private double demand;
-	private ArrayList<DistributionCenter> highPriorityMatchList = new ArrayList<>();
-	private ArrayList<DistributionCenter> lowPriorityMatchList = new ArrayList<>();
-	private DistributionCenter selectedD = null;
-
-	public DistributionCenter getSelectedD()
-	{
-		return selectedD;
+		return selectedDistributionCenter;
 	}
 
 	public String getId()
@@ -50,80 +58,80 @@ class Store
 		return id;
 	}
 
-	public void setId(String id)
-	{
-		this.id = id;
-	}
+//	public void setId(String id)
+//	{
+//		this.id = id;
+//	}
 
 	public double getX()
 	{
 		return x;
 	}
 
-	public void setX(double x)
-	{
-		this.x = x;
-	}
+//	public void setX(double x)
+//	{
+//		this.x = x;
+//	}
 
 	public double getY()
 	{
 		return y;
 	}
 
-	public void setY(double y)
-	{
-		this.y = y;
-	}
+//	public void setY(double y)
+//	{
+//		this.y = y;
+//	}
 
 	public String getLabelA()
 	{
 		return labelA;
 	}
 
-	public void setLabelA(String labelA)
-	{
-		this.labelA = labelA;
-	}
+//	public void setLabelA(String labelA)
+//	{
+//		this.labelA = labelA;
+//	}
 
 	public String getLabelO()
 	{
 		return labelO;
 	}
 
-	public void setLabelO(String labelO)
-	{
-		this.labelO = labelO;
-	}
+//	public void setLabelO(String labelO)
+//	{
+//		this.labelO = labelO;
+//	}
 
 	public String getLabelH()
 	{
 		return labelH;
 	}
 
-	public void setLabelH(String labelH)
-	{
-		this.labelH = labelH;
-	}
+//	public void setLabelH(String labelH)
+//	{
+//		this.labelH = labelH;
+//	}
 
 	public String getLabelP()
 	{
 		return labelP;
 	}
 
-	public void setLabelP(String labelP)
-	{
-		this.labelP = labelP;
-	}
+//	public void setLabelP(String labelP)
+//	{
+//		this.labelP = labelP;
+//	}
 
 	public double getDemand()
 	{
 		return demand;
 	}
 
-	public void setDemand(double demand)
-	{
-		this.demand = demand;
-	}
+//	public void setDemand(double demand)
+//	{
+//		this.demand = demand;
+//	}
 
 	public double distanceTo( DistributionCenter d )
 	{
@@ -132,9 +140,12 @@ class Store
 		return Math.sqrt(dx * dx + dy * dy);
 	}
 
-	public void configMatchList(ArrayList<DistributionCenter> DList )
+	public void configMatchList(ArrayList<DistributionCenter> dList )
 	{
-		for ( DistributionCenter d : DList)
+		// what this roughly does: If the "O" condition or the "H" condition is not fulfilled, it just moves on.  Otherwise, it adds the DC
+		// into the lowPrioMatchList, EXCEPT if the "P" condition is fulfilled.
+
+		for ( DistributionCenter d : dList)
 		{
 			boolean oFlag = this.getLabelO().equals(d.getLabelO());
 			if (!oFlag)
@@ -143,6 +154,8 @@ class Store
 			}
 
 			boolean hFlag = this.getLabelH().equals(d.getLabelH()) || this.getLabelH().equals("wild");
+			// (this is the ONLY place where getLabelH is used!)
+
 			if (!hFlag)
 			{
 				continue;
@@ -158,18 +171,18 @@ class Store
 				lowPriorityMatchList.add(d);
 			}
 		}
-		DComparator comparator = new DComparator();
+//		DComparator comparator = new DComparator();
 	}
 
 
 	//Randomly re-assignment a matching D
-	public boolean updateSelect()
+	public void updateSelect()
 	{
 		Random random = new Random();
 		ArrayList<DistributionCenter> highPriorityPendingList = new ArrayList<>();
 		for ( DistributionCenter d : this.highPriorityMatchList)
 		{
-			if (d.canAddS(this))
+			if (d.canAddStore(this ))
 			{
 				highPriorityPendingList.add(d);
 			}
@@ -179,56 +192,47 @@ class Store
 		if (hasHighPriorityMatch)
 		{
 			int index = random.nextInt(highPriorityPendingList.size());
-			return select(highPriorityPendingList.get(index));
+			selectDistributionCenter( highPriorityPendingList.get( index ) );
 		}
 		else if (hasLowPriorityMatch)
 		{
 			int index = random.nextInt(lowPriorityMatchList.size());
-			return select(lowPriorityMatchList.get(index));
-		}
-		else
-		{
-			return false;
+			selectDistributionCenter( lowPriorityMatchList.get( index ) );
 		}
 	}
 
 
 	//Select or reselect a D
-	public boolean select( DistributionCenter d )
+	private void selectDistributionCenter( DistributionCenter distributionCenter )
 	{
-		if (d == null)
+		if (distributionCenter == null)
 		{
-			clearSelect();
-			return false;
+			clearSelectedDistributionCenter();
+			return;
 		}
-		if (selectedD == d)
+		if ( selectedDistributionCenter == distributionCenter)
 		{
-			return true;
+			return;
 		}
 
-		boolean canAdd = d.addS(this);
+		boolean canAdd = distributionCenter.addStore(this );
 		if (canAdd)
 		{
-			if (selectedD != null)
+			if ( selectedDistributionCenter != null)
 			{
-				selectedD.removeS(this);
+				selectedDistributionCenter.removeStore(this );
 			}
-			this.selectedD = d;
-			return true;
-		}
-		else
-		{
-			return false;
+			this.selectedDistributionCenter = distributionCenter;
 		}
 	}
 
 
-	public void clearSelect()
+	public void clearSelectedDistributionCenter()
 	{
-		if (selectedD != null)
+		if ( selectedDistributionCenter != null)
 		{
-			selectedD.removeS(this);
-			selectedD = null;
+			selectedDistributionCenter.removeStore(this );
+			selectedDistributionCenter = null;
 		}
 	}
 
@@ -243,7 +247,7 @@ class Store
 		DistributionCenter nearestD = null;
 		for ( DistributionCenter d : this.highPriorityMatchList)
 		{
-			if (d.canAddS(this))
+			if (d.canAddStore(this ))
 			{
 				if (nearestD == null || this.distanceTo(d) < this.distanceTo(nearestD))
 				{
@@ -253,14 +257,14 @@ class Store
 		}
 		if (nearestD != null)
 		{
-			select(nearestD);
+			selectDistributionCenter(nearestD );
 			return true;
 		}
 
 		nearestD = null;
 		for ( DistributionCenter d : this.lowPriorityMatchList)
 		{
-			if (d.canAddS(this))
+			if (d.canAddStore(this ))
 			{
 				if (nearestD == null || this.distanceTo(d) < this.distanceTo(nearestD))
 				{
@@ -270,7 +274,7 @@ class Store
 		}
 		if (nearestD != null)
 		{
-			select(nearestD);
+			selectDistributionCenter(nearestD );
 			return true;
 		}
 
@@ -282,31 +286,31 @@ class Store
 
 	public void save()
 	{
-		originalSelectedIndex = selectedD;
+		originalSelectedIndex = selectedDistributionCenter;
 	}
 
 
 	public void rollback()
 	{
-		select(originalSelectedIndex);
+		selectDistributionCenter(originalSelectedIndex );
 	}
 
-	static class DComparator implements Comparator<DistributionCenter>
-	{
-		@Override
-		public int compare( DistributionCenter o1, DistributionCenter o2 )
-		{
-			double res = o1.remainingCapacity() - o2.remainingCapacity();
-			if (res < 0.0)
-			{
-				return -1;
-			}
-			else if (res > 0.0)
-			{
-				return 1;
-			}
-			return 0;
-		}
-	}
+//	static class DComparator implements Comparator<DistributionCenter>
+//	{
+//		@Override
+//		public int compare( DistributionCenter o1, DistributionCenter o2 )
+//		{
+//			double res = o1.remainingCapacity() - o2.remainingCapacity();
+//			if (res < 0.0)
+//			{
+//				return -1;
+//			}
+//			else if (res > 0.0)
+//			{
+//				return 1;
+//			}
+//			return 0;
+//		}
+//	}
 
 }
